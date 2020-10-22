d5ec467b94782bfc0575f11af8c798448db7a222
Pedro Marinho <pedro.marinho238@gmail.com>
2020-10-22 18:10:08 -0400

    listas

---
+++ js/home.js
@@ -0,0 +1,35 @@
+
+window.onload = initPage;
+
+function initPage(){
+
+    listarCategorias();
+}
+
+function paginaCategoria(valor){
+    window.location = "categoria.html?categoria="+valor;
+}
+
+function listarCategorias(){
+    const $list = $('#categorias-list');
+    var requestURL = 'https://tecflyingcommunity.github.io/json/categorias.json';
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    
+    request.onload = function() {
+    var categorias = request.response;
+    
+    for (let i = 0; i < categorias.length; i++) {
+        const categoria = categorias[i];
+
+        const type_categoria = `<button onclick="paginaCategoria(\'${categoria.nome}\')">${categoria.nome}</button>`
+        $list.append(type_categoria)
+
+        console.log(categoria.nome);
+  
+         }
+    }
+}
+
