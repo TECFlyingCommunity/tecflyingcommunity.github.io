5cb6add6c3c1d53ba6fdd3b2643095156dcc4c21
Pedro Marinho <pedro.marinho238@gmail.com>
2020-10-22 22:24:56 -0400

    ...

--- js/categoria.js
+++ js/categoria.js
@@ -1,35 +1,51 @@
-
 window.onload = initPage;
 
-function initPage(){
-
-    
+function initPage() {
+  var categoria = queryString("categoria");
+  listarCursos(categoria);
 }
 
-function paginaCurso(){
-    window.location = "curso.html";
+function paginaCurso(valor) {
+  window.location = "curso.html?curso="+valor;
 }
 
-
-function listarCursos(){
-    const $list = $('#categorias-list');
-    var requestURL = 'https://tecflyingcommunity.github.io/json/categorias.json';
-    var request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-    
-    request.onload = function() {
-    var categorias = request.response;
-    
-    for (let i = 0; i < categorias.length; i++) {
-        const categoria = categorias[i];
-
-        const type_categoria = `<button onclick="paginaCategoria(\'${categoria.nome}\')">${categoria.nome}</button>`
-        $list.append(type_categoria)
-
-        console.log(categoria.nome);
-  
-         }
+// função pra ler querystring
+function queryString(parameter) {
+  var loc = location.search.substring(1, location.search.length);
+  var param_value = false;
+  var params = loc.split("&");
+  for (i = 0; i < params.length; i++) {
+    param_name = params[i].substring(0, params[i].indexOf("="));
+    if (param_name == parameter) {
+      param_value = params[i].substring(params[i].indexOf("=") + 1);
     }
+  }
+  if (param_value) {
+    return param_value;
+  } else {
+    return undefined;
+  }
+}
+
+function listarCursos(categoria) {
+  const $list = $("#cursos-list");
+  var requestURL = "https://tecflyingcommunity.github.io/json/cursos.json";
+  var request = new XMLHttpRequest();
+  request.open("GET", requestURL);
+  request.responseType = "json";
+  request.send();
+
+  request.onload = function () {
+    var cursos = request.response;
+
+    for (let i = 0; i < cursos.length; i++) {
+      const curso = cursos[i];
+      if (curso.categoria == categoria) {
+        const type_categoria = `<button onclick="paginaCurso(\'${curso.nome_curso}\')">${curso.nome_curso}</button>`;
+        $list.append(type_categoria);
+
+        console.log(curso.nome_curso);
+      }
+    }
+  };
 }
--- js/home.js
+++ js/home.js
@@ -1,35 +1,31 @@
-
 window.onload = initPage;
 
-function initPage(){
-
-    listarCategorias();
+function initPage() {
+  listarCategorias();
 }
 
-function paginaCategoria(valor){
-    window.location = "categoria.html?categoria="+valor;
+function paginaCategoria(valor) {
+  window.location = "categoria.html?categoria=" + valor;
 }
 
-function listarCategorias(){
-    const $list = $('#categorias-list');
-    var requestURL = 'https://tecflyingcommunity.github.io/json/categorias.json';
-    var request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-    
-    request.onload = function() {
+function listarCategorias() {
+  const $list = $("#categorias-list");
+  var requestURL = "https://tecflyingcommunity.github.io/json/categorias.json";
+  var request = new XMLHttpRequest();
+  request.open("GET", requestURL);
+  request.responseType = "json";
+  request.send();
+
+  request.onload = function () {
     var categorias = request.response;
-    
+
     for (let i = 0; i < categorias.length; i++) {
-        const categoria = categorias[i];
+      const categoria = categorias[i];
 
-        const type_categoria = `<button onclick="paginaCategoria(\'${categoria.nome}\')">${categoria.nome}</button>`
-        $list.append(type_categoria)
+      const type_categoria = `<button onclick="paginaCategoria(\'${categoria.nome_categoria}\')">${categoria.nome}</button>`;
+      $list.append(type_categoria);
 
-        console.log(categoria.nome);
-  
-         }
+      console.log(categoria.nome);
     }
+  };
 }
-
