1f3f19178ca81c520e1d03abbc340913d81e015f
Riquelmy Costa <50642817+Hickelmy@users.noreply.github.com>
2020-11-04 04:20:04 -0300

    Add files via upload

--- js/code.js
+++ js/code.js
@@ -158,3 +158,48 @@ class Slideshow {
 
 	addLoadClass();
 })();
+
+
+
+jQuery(document).ready(function(){
+
+  jQuery("#subirTopo").hide();
+  
+  jQuery('dib#subirTopo').click(function () {
+           jQuery('body,html').animate({
+             scrollTop: 0
+           }, 800);
+          return false;
+     });
+  
+  jQuery(window).scroll(function () {
+           if (jQuery(this).scrollTop() > 1000) {
+              jQuery('#subirTopo').fadeIn();
+           } else {
+              jQuery('#subirTopo').fadeOut();
+           }
+       });
+  });
+
+
+
+
+
+
+
+
+
+  $(function() {
+    const $gallery = $('.gallery a').simpleLightbox();
+  });
+
+
+
+
+
+  
+
+
+
+
+
